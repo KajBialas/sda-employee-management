a64238a0a5c9581c4f6f3ebed57de36b49c6df4a
Jacek Kalinowski <jacekkalinowski27@gmail.com>
2023-10-29 13:02:25 +0100

    FEAT/EM-9/create_home_layout

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,5 +1,18 @@
+import { useContext } from "react";
 import "./Home.scss";
 
 export const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <h1 className="home__title">Welcome to the employee-management App</h1>
+      <p className="home__employees">Number of activ Employees: {10} </p>
+      <h4 className="homepage__text">Employees List</h4>
+      <button type="submit">Go to List</button>
+      <Homepage />
+    </div>
+  );
+};
+
+export const Homepage = () => {
+  return <div>Go</div>;
 };
