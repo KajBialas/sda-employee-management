64ab028607cdd294e822612d9bb5016a4e356b6d
Krzysztof <krzysztoflesiak99@gmail.com>
2023-10-29 11:34:38 +0100

    EM-5/Create-routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" />;
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
